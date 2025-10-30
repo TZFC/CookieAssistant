1572d3540b969224ff9f1fc29e814ffbb2a7befd
天子方辰 <tianzifangchen@gmail.com>
2025-10-30 02:11:22 -0400

    Simplfy spell cast logic

--- main.js
+++ main.js
@@ -511,22 +511,14 @@ CookieAssistant.launch = function()
 						}
 						var grimoire = Game.ObjectsById[7].minigame;
 						var spell = grimoire.spells['hand of fate'];
-						var cost = 0;
-						// Calculate both costs once
-						const costMin = Math.floor(spell.costMin + grimoire.magicM * spell.costPercent);
-						const costMax = grimoire.magicM;
 						
 						// Conditions
-						const satisfyMinMP = costMin <= grimoire.magic;
-						const reachMaxMP   = costMax <= grimoire.magic;
-						
-						// Buff conditions
-						const hasAtLeast1Buff = buffCount >= 1;
-						const hasAtLeast2Buff = buffCount >= 2;
+						const satisfyMinMP = Math.floor(spell.costMin + grimoire.magicM * spell.costPercent) <= grimoire.magic;
+						const reachMaxMP   = grimoire.magicM === grimoire.magic;
 						
 						// Trigger if:
 						// (minimum MP & ≥2 buffs) OR (max MP & ≥1 buff)
-						if ((satisfyMinMP && hasAtLeast2Buff) || (reachMaxMP && hasAtLeast1Buff)) {
+						if ((satisfyMinMP && buffCount >= 2) || (reachMaxMP && buffCount >= 1)) {
 						    grimoire.castSpell(spell);
 						    CookieAssistant.isAfterSpellcast = true;
 						    setTimeout(() => {
